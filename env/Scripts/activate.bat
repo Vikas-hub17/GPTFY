5db015e131c11331bb1aac97de619d76f2063b4b
VIKAS <vikasraw17@gmail.com>
2024-11-29 13:29:20 +0530

    commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vikas\OneDrive\Desktop\GPTFY\GPTFY\env
+set VIRTUAL_ENV=C:\Users\vikas\OneDrive\Desktop\GPTFY\env
 
 if not defined PROMPT set PROMPT=$P$G
 
