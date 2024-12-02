a582c274092eb4a3d827e2e5d2f9c053ff1ab55e
VIKAS <vikasraw17@gmail.com>
2024-12-02 10:16:04 +0530

    commit

--- graph_db/graph_queries.py
+++ graph_db/graph_queries.py
@@ -2,7 +2,14 @@ from neo4j import GraphDatabase
 
 class GraphDatabaseHandler:
     def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self._uri = uri
+        self._user = 'neo4j'
+        self._password = 12345678
+        self._driver = None
+        self._connect()
+    
+    def _connect(self):
+        self._driver = GraphDatabase.driver(self._uri, auth=(self._user, self._password))
 
     def close(self):
         self.driver.close()
@@ -10,6 +17,11 @@ class GraphDatabaseHandler:
     def query_articles(self):
         with self.driver.session() as session:
             return session.read_transaction(self._query_articles)
+        
+    def _query_articles(self, tx):
+        query = "MATCH (a:Article) RETURN a.title"
+        result = tx.run(query)
+        return [record["a.title"] for record in result]  
 
     @staticmethod
     def _query_articles(tx):
